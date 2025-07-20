4462b322b6f07574445bbaf9f09343fe735e0fb0
guychenya <guy.chenya@gmail.com>
2025-07-20 12:44:11 +0300

    Add untracked files

---
+++ .wrangler/tmp/bundle-lkpjGX/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "/Users/guychenya/Documents/GitHub-Repos/bolt.diy/.wrangler/tmp/pages-J747iX/functionsWorker-0.4435738361195797.mjs";
+				import * as __MIDDLEWARE_0__ from "/Users/guychenya/Documents/GitHub-Repos/bolt.diy/node_modules/.pnpm/wrangler@3.91.0_@cloudflare+workers-types@4.20241127.0/node_modules/wrangler/templates/middleware/middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "/Users/guychenya/Documents/GitHub-Repos/bolt.diy/node_modules/.pnpm/wrangler@3.91.0_@cloudflare+workers-types@4.20241127.0/node_modules/wrangler/templates/middleware/middleware-miniflare3-json-error.ts";
+
+				export * from "/Users/guychenya/Documents/GitHub-Repos/bolt.diy/.wrangler/tmp/pages-J747iX/functionsWorker-0.4435738361195797.mjs";
+
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
---
+++ .wrangler/tmp/pages-J747iX/functionsRoutes-0.4181136636959193.mjs
@@ -0,0 +1,11 @@
+import { onRequest as ____path___ts_onRequest } from "/Users/guychenya/Documents/GitHub-Repos/bolt.diy/functions/[[path]].ts"
+
+export const routes = [
+    {
+      routePath: "/:path*",
+      mountPath: "/",
+      method: "",
+      middlewares: [],
+      modules: [____path___ts_onRequest],
+    },
+  ]
